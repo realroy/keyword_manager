d3ac6a22e7445d2128dd920c0700601202f5f650
realroy <roiboon.cc@gmail.com>
2023-07-20 02:07:30 +0700

    feat(controllers): add logic to parse and create new keywords form user uploaded file

--- app/controllers/keywords/uploads_controller.rb
+++ app/controllers/keywords/uploads_controller.rb
@@ -4,6 +4,24 @@ module Keywords
   class UploadsController < ApplicationController
     def show; end
 
-    def update; end
+    def update
+      raw_file = uploads_params[:file].read
+      words = raw_file.split("\n").map(&:strip)
+      new_words = words - Keyword.pluck(:word)
+
+      ActiveRecord::Base.transaction do
+        new_words.each { |new_word| current_user.keywords.create! word: new_word }
+      end
+
+      redirect_to keywords_path
+    rescue StandardError => e
+      p e
+      flash[:error] = 'Something went wrong! Please try again.'
+      render 'show'
+    end
+
+    def uploads_params
+      params.require(:uploads).permit(:file)
+    end
   end
 end
