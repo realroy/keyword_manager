cdea646c302d3d3d69e1a474afd9c38d5b9ab2f8
realroy <roiboon.cc@gmail.com>
2023-07-19 23:25:30 +0700

    feat: add keywords controller, routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :keywords, only: %w[index show]
+
+  root 'keywords#index'
 end
