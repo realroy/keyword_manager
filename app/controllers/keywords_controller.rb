822650af924c618c0caa5fb3cbc67dcf1c6a09f9
realroy <roiboon.cc@gmail.com>
2023-07-20 00:18:02 +0700

    feat(controller): update logic keywords #index and #show

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -3,7 +3,15 @@
 class KeywordsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @keywords = if params[:q].present?
+                  current_user.keywords.where('name ILIKE ?', "%#{params[:q]}%")
+                else
+                  current_user.keywords
+                end
+  end
 
-  def show; end
+  def show
+    @keyword = current_user.keywords.find(params[:id])
+  end
 end
