4addf327752c1c399e7ba0aa47155b196759db0e
realroy <roiboon.cc@gmail.com>
2023-07-20 02:21:57 +0700

    refactor(controllers): keywords/uploads#update

--- app/controllers/keywords/uploads_controller.rb
+++ app/controllers/keywords/uploads_controller.rb
@@ -5,23 +5,30 @@ module Keywords
     def show; end
 
     def update
-      raw_file = uploads_params[:file].read
-      words = raw_file.split("\n").map(&:strip)
-      new_words = words - Keyword.pluck(:word)
-
-      ActiveRecord::Base.transaction do
-        new_words.each { |new_word| current_user.keywords.create! word: new_word }
-      end
-
-      redirect_to keywords_path
+      new_words = extract_words_form_upload_file - Keyword.pluck(:word)
+      create_new_keywords(new_words)
     rescue StandardError => e
       p e
       flash[:error] = 'Something went wrong! Please try again.'
       render 'show'
     end
 
+    private
+
     def uploads_params
       params.require(:uploads).permit(:file)
+    end
+
+    def extract_words_form_upload_file
+      raw_file = uploads_params[:file].read
+
+      raw_file.split("\n").map(&:strip)
+    end
+
+    def create_new_keywords(new_words)
+      ActiveRecord::Base.transaction do
+        new_words.each { |new_word| current_user.keywords.create! word: new_word }
+      end
     end
   end
 end
