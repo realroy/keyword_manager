19ecd384460d0ff37c69beabc115f603902c6246
realroy <roiboon.cc@gmail.com>
2023-07-20 13:03:48 +0700

    chore(specs): add factories

---
+++ spec/factories/keyword.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :keyword do
+    word { Faker::Book.unique.title }
+    total_link { (Random.rand * 10).round }
+    total_adword { |keyword| (Random.rand * keyword.total_link).round }
+    total_result { (Random.rand * 1_000_000_000).round }
+    total_search_time { Random.rand }
+    html { nil }
+  end
+end
---
+++ spec/factories/user_keyword.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user_keyword do
+    user
+    keyword
+  end
+end
