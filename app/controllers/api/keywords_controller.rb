1922e65b9763d1353077547c5133d2fdc0f0f001
realroy <roiboon.cc@gmail.com>
2023-07-20 20:52:04 +0700

    test(controller): mock api/keywords

--- app/controllers/api/keywords_controller.rb
+++ app/controllers/api/keywords_controller.rb
@@ -2,8 +2,16 @@
 
 module Api
   class KeywordsController < ApplicationController
-    def index; end
+    def index
+      keywords = GetKeywordsForUserService.new(user: User.first, q: params[:q]).call
 
-    def show; end
+      render json: keywords
+    end
+
+    def show
+      keyword = GetKeywordForUserService.new(user: User.first, keyword_id: params[:id]).call
+
+      render json: keyword
+    end
   end
 end
