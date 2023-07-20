8599d198fe06c371d63a086e9938a9c28764a3eb
realroy <roiboon.cc@gmail.com>
2023-07-20 22:17:09 +0700

    chore: add spec

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,15 +1,32 @@
 module Api
+  class UnauthorizedError < StandardError; end
+
   class BaseController < ApplicationController
     protect_from_forgery with: :null_session
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from UnauthorizedError, with: :render_unauthorized
 
     protected
 
     def current_user!
+      raise UnauthorizedError, 'Missing authorization header' if request.headers['Authorization'].nil?
+
       token = request.headers['Authorization'].split(' ').last
       payload = JWT.decode(token, 'SECRET').first
-      @current_user ||= User.find(payload['sub'])
-    rescue StandardError
-      render json: { error: 'Invalid access token', status: :unauthorized }, status: :unauthorized
+      User.find(payload['sub'])
+    rescue ActiveRecord::RecordNotFound
+      raise UnauthorizedError, 'Invalid authorization token'
+    end
+
+    private
+
+    def render_unauthorized(err)
+      render json: { error: err.message, statusCode: :unauthorized }, status: :unauthorized
+    end
+
+    def render_not_found(err)
+      render json: { error: err.message, statusCode: :not_found }, status: :not_found
     end
   end
 end
--- spec/requests/api/keywords_spec.rb
+++ spec/requests/api/keywords_spec.rb
@@ -1,19 +1,71 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'jwt'
 
 RSpec.describe 'Api::Keywords', type: :request do
+  let(:user) { create(:user) }
+  let(:keywords) { create_list(:keyword, 3) }
+
+  let(:access_token) do
+    JWT.encode({ sub: user.id }, 'SECRET')
+  end
+
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/api/keywords/index'
-      expect(response).to have_http_status(:success)
+    context 'when user doesn\'t loged in' do
+      it 'returns http unauthorized' do
+        get api_keywords_path
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when user loged in' do
+      before do
+        keywords.each { |keyword| create(:user_keyword, keyword:, user:) }
+
+        get api_keywords_path,
+            headers: { 'Authorization': "Bearer #{access_token}" }
+      end
+
+      it 'returns http ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'return keywords' do
+        expect(response.body).to eq(keywords.to_json)
+      end
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/api/keywords/show'
-      expect(response).to have_http_status(:success)
+    context 'when user doesn\'t logged in' do
+      it 'returns http unauthorized' do
+        get api_keyword_path(keywords.first.id)
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when keyword is not found' do
+      it 'returns http not found' do
+        get api_keyword_path(-1),
+            headers: { 'Authorization': "Bearer #{access_token}" }
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'when keyword is found' do
+      before do
+        keywords.each { |keyword| create(:user_keyword, keyword:, user:) }
+      end
+      it 'returns http ok' do
+        get api_keyword_path(keywords.first.id),
+            headers: { 'Authorization': "Bearer #{access_token}" }
+
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
