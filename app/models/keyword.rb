eeaf5f4e056e59cf2c680301dbbfc1c7512b95b3
realroy <roiboon.cc@gmail.com>
2023-07-23 22:06:31 +0700

    fix(models): issues

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Keyword < ApplicationRecord
-  SCRAPE_STATUSES = { pending: 'PENDING', sucess: 'SUCCESS', failed: 'FAILED' }.freeze
-
-  enum scrape_status: SCRAPE_STATUSES, default: SCRAPE_STATUSES[:pending]
+  enum scrape_status: { pending: 'PENDING', sucess: 'SUCCESS', failed: 'FAILED' }, _default: 'PENDING'
 
   has_many :user_keywords, inverse_of: :keyword, dependent: :nullify
   has_many :user, through: :user_keywords
 
-  validate :word, presence: true, uniqueness: true
+  validates :word, presence: true, uniqueness: true
 end
