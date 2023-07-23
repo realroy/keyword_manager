e431a01d0b73f47894ab592b5efd1fbdba29aa93
realroy <roiboon.cc@gmail.com>
2023-07-24 06:17:14 +0700

    fix: merge conflict

--- app/controllers/keywords/uploads_controller.rb
+++ app/controllers/keywords/uploads_controller.rb
@@ -12,7 +12,7 @@ module Keywords
       @keyword_upload_file = KeywordUploadFile.new(file: uploads_params[:file])
       if @keyword_upload_file.valid?
         keywords = ExtractKeywordsFromFileService.new(words: @keyword_upload_file.words, user: current_user).call
-        keywords.each { |keyword| ScrapeFromKeywordService.new(keyword:).call }
+        ScrapeKeywordsJob.perform_async(keywords.map(&:id))
 
         redirect_to keywords_path, notice: 'Keywords is starting to scrape'
       else
