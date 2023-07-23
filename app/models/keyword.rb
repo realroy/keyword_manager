d2eaaa20eb00992d03307da84972a46c0d560679
realroy <roiboon.cc@gmail.com>
2023-07-23 21:56:54 +0700

    feat(models): add enum in keyword model

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Keyword < ApplicationRecord
+  SCRAPE_STATUSES = { pending: 'pending', success: 'success', failed: 'failed' }.freeze
+
+  enum scrape_status: SCRAPE_STATUSES, default: SCRAPE_STATUSES[:pending]
+
   has_many :user_keywords, inverse_of: :keyword, dependent: :nullify
   has_many :user, through: :user_keywords
 end
